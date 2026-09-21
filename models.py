06863d06a02e5bee441b03be890efc0093ad7ec7
Frank-1mbuki <frankimbuki03@gmail.com>
2026-09-21 16:45:11 +0400

    Fix Member __str__ formatting in models.py

--- models.py
+++ models.py
@@ -4,16 +4,5 @@ class Member:
         self.name = name
         self.email = email
 
-class Equipment:
-    def __init__(self, equipment_id, name, category, status):
-        self.equipment_id = equipment_id
-        self.name = name
-        self.category = category
-        self.status = status
-
-class Loan:
-    def __init__(self, loan_id, member_id, equipment_id, checkout_date):
-        self.loan_id = loan_id
-        self.member_id = member_id
-        self.equipment_id = equipment_id
-        self.checkout_date = checkout_date
+    def __str__(self):
+        return f"ID: {self.member_id} | Name: {self.name} | Email: {self.email}"
